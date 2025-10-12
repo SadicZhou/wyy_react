95b1a0405e759b2d94fe60c53928df925d9ae69b
zhoujianhao <1372819785@qq.com>
2025-10-12 21:59:49 +0800

    初始化

---
+++ ts_react_music/craco.config.js
@@ -0,0 +1,20 @@
+const CracoLessPlugin = require("craco-less");
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+  webpack: {
+    alias: {
+      "@": require("path").resolve(__dirname, "./src"),
+    },
+  },
+};
---
+++ ts_react_music/src/index.d.ts
@@ -0,0 +1,8 @@
+declare module '*.scss' {
+    const content: { [key: string]: any }
+    export = content
+}
+declare module '*.less' {
+    const content: { [key: string]: any }
+    export = content
+}
